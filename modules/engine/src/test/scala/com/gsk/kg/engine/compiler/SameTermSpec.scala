6723bf95cc7c1bb040c965545f12d024e35c25da
Patricio <patofp@gmail.com>
2021-06-03 15:50:05 +0200

    added integration test on unrelated typed literals

--- modules/engine/src/test/scala/com/gsk/kg/engine/compiler/SameTermSpec.scala
+++ modules/engine/src/test/scala/com/gsk/kg/engine/compiler/SameTermSpec.scala
@@ -61,6 +61,82 @@ class SameTermSpec
           Row("\"Ms A.\"", "\"Alice\"")
         )
       }
+
+      "simple query with non-equivalent typed literals" in {
+
+        val df: DataFrame = List(
+          (
+            "_:c1",
+            "<http://example.org/WMterms#label>",
+            "Container 1"
+          ),
+          (
+            "_:c1",
+            "<http://example.org/WMterms#weight>",
+            "\"100\"^^<http://example.org/types#kilos>"
+          ),
+          (
+            "_:c1",
+            "<http://example.org/WMterms#displacement>",
+            "\"100\"^^<http://example.org/types#liters>"
+          ),
+          (
+            "_:c2",
+            "<http://example.org/WMterms#label>",
+            "Container 2"
+          ),
+          (
+            "_:c2",
+            "<http://example.org/WMterms#weight>",
+            "\"100\"^^<http://example.org/types#kilos>"
+          ),
+          (
+            "_:c2",
+            "<http://example.org/WMterms#displacement>",
+            "\"85\"^^<http://example.org/types#liters>"
+          ),
+          (
+            "_:c3",
+            "<http://example.org/WMterms#label>",
+            "Container 3"
+          ),
+          (
+            "_:c3",
+            "<http://example.org/WMterms#weight>",
+            "\"85\"^^<http://example.org/types#kilos>"
+          ),
+          (
+            "_:c3",
+            "<http://example.org/WMterms#displacement>",
+            "\"85\"^^<http://example.org/types#liters>"
+          )
+        ).toDF("s", "p", "o")
+
+        val query =
+          """
+            |PREFIX  :      <http://example.org/WMterms#>
+            |PREFIX  t:     <http://example.org/types#>
+            |
+            |SELECT ?aLabel ?bLabel
+            |WHERE { ?a  :label        ?aLabel .
+            |        ?a  :weight       ?aWeight .
+            |        ?a  :displacement ?aDisp .
+            |
+            |        ?b  :label        ?bLabel .
+            |        ?b  :weight       ?bWeight .
+            |        ?b  :displacement ?bDisp .
+            |        FILTER ( sameTerm(?aWeight, ?bWeight) && !sameTerm(?aDisp, ?bDisp)) }
+            |""".stripMargin
+
+        val result = Compiler.compile(df, query, config)
+
+        result shouldBe a[Right[_, _]]
+        result.right.get.collect.length shouldEqual 2
+        result.right.get.collect.toSet shouldEqual Set(
+          Row("\"Container 1\"", "\"Container 2\""),
+          Row("\"Container 2\"", "\"Container 1\"")
+        )
+      }
     }
   }
 }
