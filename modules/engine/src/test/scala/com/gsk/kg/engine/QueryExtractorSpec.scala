c1e332dd4346b89d040ce9949fc0e2f349be4b22
Pepe García <pepe@pepegar.com>
2021-06-08 10:56:40 +0200

    test code works with UNION

--- modules/engine/src/test/scala/com/gsk/kg/engine/QueryExtractorSpec.scala
+++ modules/engine/src/test/scala/com/gsk/kg/engine/QueryExtractorSpec.scala
@@ -131,5 +131,40 @@ FROM <http://gsk-kg.rdip.gsk.com/dm/1.0/ccc>
 FROM <http://gsk-kg.rdip.gsk.com/dm/1.0/semmed>
  WHERE { ?d <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://gsk-kg.rdip.gsk.com/dm/1.0/Document> . ?d <http://gsk-kg.rdip.gsk.com/dm/1.0/contains> ?ds . ?ds <http://gsk-kg.rdip.gsk.com/dm/1.0/contains> ?te . BIND(STRAFTER(STR(?d), "#") as ?docid) }"""
     }
+
+    "handle GRAPH unions correctly" in {
+      val query = """
+        PREFIX  dm:   <http://gsk-kg.rdip.gsk.com/dm/1.0/>
+        CONSTRUCT {
+          ?te dm:contains ?docid .
+        }
+        WHERE {
+           { GRAPH <http://gsk-kg.rdip.gsk.com/dm/1.0/Semmed?type=snapshot&t=20200309>
+             {
+               ?ds dm:contains ?te .
+             } 
+           } UNION {
+             GRAPH <http://gsk-kg.rdip.gsk.com/dm/1.0/Elsevier?type=incremental&t=20200109>
+             {
+               ?d a dm:Document .
+               ?d dm:contains ?ds .
+             }
+           }
+           BIND(STRAFTER(str(?d), "#") as ?docid) .
+        }"""
+
+      val result = QueryExtractor.extractInfo(query)
+
+      result._2 shouldEqual Map(
+        "http://gsk-kg.rdip.gsk.com/dm/1.0/Elsevier" ->
+          List(QueryParam("type", "incremental"), QueryParam("t", "20200109")),
+        "http://gsk-kg.rdip.gsk.com/dm/1.0/Semmed" ->
+          List(QueryParam("type", "snapshot"), QueryParam("t", "20200309"))
+      )
+
+      result._1 shouldEqual """CONSTRUCT { ?te <http://gsk-kg.rdip.gsk.com/dm/1.0/contains> ?docid . }
+
+ WHERE { { GRAPH <http://gsk-kg.rdip.gsk.com/dm/1.0/Semmed> { ?ds <http://gsk-kg.rdip.gsk.com/dm/1.0/contains> ?te . } } UNION { GRAPH <http://gsk-kg.rdip.gsk.com/dm/1.0/Elsevier> { ?d <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://gsk-kg.rdip.gsk.com/dm/1.0/Document> . ?d <http://gsk-kg.rdip.gsk.com/dm/1.0/contains> ?ds . } } BIND(STRAFTER(STR(?d), "#") as ?docid) }"""
+    }
   }
 }
