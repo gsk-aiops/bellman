3e328d578a372405cc88d31e6776e9ebc666d711
Juan Manuel García Navarro <juan.garcia@47deg.com>
2021-07-01 15:17:57 +0200

    Add unify function to evaluate tests

--- modules/engine/src/test/scala/com/gsk/kg/engine/compiler/package.scala
+++ modules/engine/src/test/scala/com/gsk/kg/engine/compiler/package.scala
@@ -1,13 +1,22 @@
 package com.gsk.kg.engine
 
+import com.gsk.kg.sparqlparser.TestConfig
+
 import org.apache.jena.riot.RDFParser
 import org.apache.jena.riot.lang.CollectorStreamTriples
 
+import org.apache.spark.sql.Column
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.Row
 import org.apache.spark.sql.SQLContext
+
+import org.scalatest.Assertion
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
 package object compiler {
 
-  def readNTtoDF(path: String)(implicit sc: SQLContext) = {
+  def readNTtoDF(path: String)(implicit sc: SQLContext): DataFrame = {
 
     import scala.collection.JavaConverters._
     import sc.implicits._
@@ -30,4 +39,39 @@ package object compiler {
       .toDF("s", "p", "o")
   }
 
+  object Evaluation
+      extends AnyWordSpec
+      with Matchers
+      with SparkSpec
+      with TestConfig {
+
+    val renamedColumn = "c1"
+
+    def eval(
+        df: DataFrame,
+        projection: Option[Column],
+        query: String,
+        expected: List[Row]
+    ): Assertion = {
+      val result = Compiler.compile(df, query, config)
+
+      val dfR: DataFrame = result match {
+        case Left(e)  => throw new Exception(e.toString)
+        case Right(r) => r
+      }
+      dfR.show(false)
+
+      projection
+        .map(p =>
+          dfR
+            .withColumnRenamed(dfR.columns.head, renamedColumn)
+            .select(
+              p
+            )
+        )
+        .getOrElse(dfR)
+        .collect()
+        .toList shouldEqual expected
+    }
+  }
 }
