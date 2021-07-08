ca099e8bedd9b913ab688d10969983a4d10423bd
zera <analyticphilosophy@gmail.com>
2021-07-08 09:25:22 -0500

    fix test (pass str not var)

--- modules/engine/src/test/scala/com/gsk/kg/engine/compiler/StrlangSpec.scala
+++ modules/engine/src/test/scala/com/gsk/kg/engine/compiler/StrlangSpec.scala
@@ -20,19 +20,17 @@ class StrlangSpec
 
     "tag is es" in {
       val str = "chat"
-      val tag      = "es"
-      val lit = quote(str)
-      val expected = Row(s"$lit@$tag")
-      val actual = act(str, tag)
+      val tag = "es"
+      val expected = Row(s"${quote(str)}@$tag")
+      val actual = act(str, quote(tag))
       actual shouldEqual expected
     }
 
     "tag is en-US" in {
       val str = "chat"
-      val tag      = "en-US"
-      val lit = quote(str)
-      val expected = Row(s"$lit@$tag")
-      val actual = act(str, tag)
+      val tag = "en-US"
+      val expected = Row(s"${quote(str)}@$tag")
+      val actual = act(str, quote(tag))
       actual shouldEqual expected
     }
 
@@ -44,31 +42,27 @@ class StrlangSpec
     val df = List(
       (
         "<http://uri.com/subject/#a1>",
-        "<http://xmlns.com/foaf/0.1/title>",
-        tag
-      ),
-      (
-        "<http://uri.com/subject/#a1>",
         "<http://xmlns.com/foaf/0.1/name>",
         str
       )
     ).toDF("s", "p", "o")
 
     val query =
-      """
+      s"""
          |PREFIX foaf: <http://xmlns.com/foaf/0.1/>
          |SELECT ?strlang
          |WHERE {
-         |?x foaf:title ?title .
          |?x foaf:name ?name .
-         |BIND(strlang(?name, ?title) as ?strlang)
+         |BIND(strlang(?name, $tag) as ?strlang)
          |}""".stripMargin
 
-    Compiler
-      .compile(df, query, config)
-      .right
-      .get
-      .head()
+    print(query)
+
+    (Compiler
+      .compile(df, query, config) match {
+      case Left(l) => throw new Exception(l.toString)
+      case Right(r) => r
+    }).head()
 
   }
 }
